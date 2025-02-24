2bf9c83ef0febf7044bf60a1a7dffd99b2b7b8c9
nartaaaa <167331196+nartaaaa@users.noreply.github.com>
2025-02-24 17:43:45 +0100

    ...

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+// $x=120;
+// $y=50;
+// echo $x + $y;
+// echo $x - $y;
+// echo $x * $y;
+// echo $x / $y;
+
+// $a = 'Digital ';
+// $b = 'School ';
+// $c = $a.$b;
+
+// echo $c;
+
+// $the_string = 'Digital school';
+
+// echo strlen($the_string);
+
+// echo '<br>';
+// $the_string1='Digital school';
+// echo str_word_count($the_string1);
+
+$programming = 'Programming is not cool';
+echo str_replace("not", "very", "$programming");
+echo '<br>';
+$the_string='Senad';
+echo strrev($the_string);
+
+
+$programming = 'Rona is not a good student';
+echo str_replace("not", "very", "$programming");
+echo '<br>';
+$the_string='Rona';
+echo strrev($the_string);
+?>
